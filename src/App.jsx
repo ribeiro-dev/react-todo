c9c8e2908d38c313443d0e95ebfcfa830dd0186e
ribeiro-dev <romilson.2r@gmail.com>
2023-07-15 13:22:29 -0300

    add: todos form submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
+import { useState } from "react"
+
 import "./styles.css"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false
+        }
+      ]
+    })
+
+    setNewItem("");
+  }
+
   return (
     <>
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">Novo item</label>
-        <input type="text" name="item" id="item" />
-      </div>
-      <button className="btn">Add</button>
-    </form>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">Novo item</label>
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" name="item" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
 
-    <h1 className="header">Lista de Todos:</h1>
-    <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox" />
-          Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-    
-      <li>
-        <label htmlFor="todo">
-          <input type="checkbox" />
-          Item 2
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-    </ul>
+      <h1 className="header">Lista de Todos:</h1>
+      <ul className="list">
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
